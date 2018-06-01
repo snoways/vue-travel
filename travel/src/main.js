c7a629eb8ef2564173c8ebcb41e37d7bd680d1b7
zhangcong <zhangcong@sz.hitrontech.com>
2018-06-01 15:23:41 +0800

    初始化项目及文件夹. 添加reset.css 和border.css. 引入解决移动端点击事件演示fastClick.

--- travel/src/main.js
+++ travel/src/main.js
@@ -2,9 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
+import fastClick from "fastclick";
+import "./assets/style/reset.css";
+import "./assets/style/border.css";
 Vue.config.productionTip = false;
-
+// 解决移动端 点击按钮延迟300ms问题
+fastClick.attach(document.body);
 new Vue({
   router,
   store,
---
+++ travel/src/pages/Home/home.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="home">Home</div>
+</template>
+
+<script>
+    export default {
+        name:'home'
+    }
+</script>
+<style>
+.home{
+    font-size: 50px;
+}
+</style>
