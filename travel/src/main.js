cc6da0852b8a58b925ff3f98b5d2a3930849370e
zhangcong <zhangcong@sz.hitrontech.com>
2018-06-02 17:55:32 +0800

    首页头部

--- travel/src/main.js
+++ travel/src/main.js
@@ -5,6 +5,7 @@ import store from "./store";
 import fastClick from "fastclick";
 import "./assets/style/reset.css";
 import "./assets/style/border.css";
+import "./assets/style/iconfont.css";
 Vue.config.productionTip = false;
 // 解决移动端 点击按钮延迟300ms问题
 fastClick.attach(document.body);
--- travel/src/pages/Home/component/Header.vue
+++ travel/src/pages/Home/component/Header.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="header">
-        <div class="header-left">返回</div>
+        <div class="header-left"><span class="iconfont back-icon">&#xe697;</span></div>
         <div class="header-input">请输入城市/景点/游玩主题</div>
-        <div class="header-right">城市</div>
+        <div class="header-right">城市 <span class="iconfont down-icon">&#xe6a6;</span> </div>
     </div>
 </template>
 
@@ -21,6 +21,9 @@
   .header-left
     width: .64rem
     float: left 
+    .back-icon
+      text-align center  
+      font-size .4rem
   .header-input
     flex 1 
     background #fff
@@ -33,5 +36,8 @@
   .header-right
     width: 1.24rem
     float: right
-    text-align center  
+    text-align center
+    .down-icon
+      text-align center
+      font-size .4rem  
 </style>
